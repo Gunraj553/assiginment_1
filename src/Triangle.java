79762fa6458a74ae7a6f242f7ef47dfdb6c47821
Gunraj553 <gsodhi1557@hotmail.com>
2023-10-20 18:38:17 -0400

    assignment 1

---
+++ src/Triangle.java
@@ -0,0 +1,29 @@
+public class Triangle {
+   Point side_1;
+   Point side_2;
+
+   Point side_3;
+
+    public void setSide_1(Point side_1) {
+        this.side_1 = side_1;
+    }
+    public void setSide_2(Point side_2){
+        this.side_2 = side_2;
+    }
+
+    public void setSide_3(Point side_3) {
+        this.side_3 = side_3;
+    }
+
+    public Point getSide_1() {
+        return side_1;
+    }
+
+    public Point getSide_2() {
+        return side_2;
+    }
+
+    public Point getSide_3() {
+        return side_3;
+    }
+}
